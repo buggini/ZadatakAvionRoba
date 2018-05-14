d618a4e5262481a7efb38e8e443346d373d8c959
buggini <zeljanzivkov@gmail.com>
2018-05-14 20:54:53 +0200

    Uradjen zadatak dodele vrednosti closes #8

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,18 +1,46 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import modeli.Avion;
+import modeli.Roba;
+
+import java.util.List;
 
 public class Zadatak3IzmenaVrednosti {
+
+    static Dao<Avion, Integer> avionDao;
+    static Dao<Roba, Integer> robaDao;
+
     public static void main(String[] args) {
 
         ConnectionSource connectionSource =null;
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
 
+            List<Roba> roba = robaDao.queryForAll();
+
+            for (Roba rb : roba) {
+                System.out.println(rb);
+
+            }
+
+            List<Roba> robas =robaDao.queryForEq(Roba.POLJE_OPIS, "Plasticna stolica");
+
+
+            Roba r = robas.get(0);
+
+            r.setOpis("Drvena Stolica");
+
+            robaDao.update(r);
 
         }catch (Exception e){
 
         }
+
+
+
+
     }
 }
